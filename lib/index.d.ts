0eda11d023e6c574ced743a080423c4d92d51300
Angel Johank <angeljohank@gmail.com>
2022-07-23 17:35:07 -0600

    testing lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,8 @@
 import { get } from './methods/get';
 import { post } from './methods/post';
-export declare const rest: {
+declare const rest: {
     get: typeof get;
     post: typeof post;
 };
+export default rest;
 //# sourceMappingURL=index.d.ts.map
